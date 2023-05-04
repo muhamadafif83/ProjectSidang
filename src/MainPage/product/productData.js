732e8c5416e96e83e930a3359e6d9e8003939889
muhamad afif mustafa <muhamadafifmustafa83@gmail.com>
2023-05-05 06:52:08 +0700

    final 2.1

--- src/MainPage/product/productData.js
+++ src/MainPage/product/productData.js
@@ -1,22 +1,22 @@
 const data = [
-    {
-      id: "1",
-      img: "./Image/rompi anak bahan pradut tebal.jpg",
-      title: "Rompi anak bahan paradut tebal",
-      size: "size : 1 - 3 thn",
-      harga: "Rp.40.000",
-      condition: 'Good condition Tag Brand atas',
-      desc: "Kontak kami di Whatsapp dan Instagram di bawah untuk info lebih lanjut",
-    },
-    {
-      id: "2",
-      img: "./Image/rompi bulu.jpg",
-      title: "Rompi bulu",
-      size: "size : 1 - 3 thn",
-      harga: "Rp.35.000",
-      condition: 'Good condition Tag Brand atas',
-      desc: "Kontak kami di Whatsapp dan Instagram di bawah untuk info lebih lanjut",
-    },
-  ];
-  
-  export default data;
+  {
+    id: "1",
+    img: "./Image/rompi anak bahan pradut tebal.jpg",
+    title: "Rompi anak bahan paradut tebal",
+    size: "size : 1 - 3 thn",
+    harga: "Rp.40.000",
+    condition: "Good condition Tag Brand atas",
+    desc: "Baju ini bisa membuat anak anda lebih hangat ",
+  },
+  {
+    id: "2",
+    img: "./Image/rompi bulu.jpg",
+    title: "Rompi bulu",
+    size: "size : 1 - 3 thn",
+    harga: "Rp.35.000",
+    condition: "Good condition Tag Brand atas",
+    desc: "Baju ini bisa membuat anak anda lebih hangat dengan bahan bulu yang hangat",
+  },
+];
+
+export default data;
